763ea80deaa7f17f1c5381f10199313fc837a856
Mercedes Flowers <mercedes.flowers@kellogg.northwestern.edu>
2023-05-17 03:24:44 +0000

    Added first post

--- scripts/create_data.rb
+++ scripts/create_data.rb
@@ -1,8 +1,16 @@
 Place.destroy_all
+Post.destroy_all
 
 mex = Place.new
 mex["name"] = "Mexico City"
 mex.save
+
+tacos = Post.new
+tacos["title"] = "Ate tacos"
+tacos["posted_on"] = Date.today
+tacos["description"] = "Best I ever had."
+tacos["place_id"] = mex["id"]
+tacos.save
 
 char = Place.new
 char["name"] = "Charleston"
