d98c8de248059efab275a5c828f3c891b7be2a68
Mercedes Flowers <mercedes.flowers@kellogg.northwestern.edu>
2023-05-17 02:36:04 +0000

    Refined places code

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,4 +4,19 @@ class PlacesController < ApplicationController
     @places = Place.all
   end
 
+  def show
+    
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new
+    @place["name"] = params["place"]["name"]
+  @place.save
+    redirect_to "/places"
+    end
+
 end
