bd0409bbae27a0744a2739348d6f23625149c600
Mercedes Flowers <mercedes.flowers@kellogg.northwestern.edu>
2023-05-17 01:31:20 +0000

    Established controllers and index views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources "places"
   resources "posts"
-
+  resources "places"
+  
   # Defines the root path route ("/")
-  get("/", { :controller => "posts", :action => "index" })
+  #get("/", { :controller => "posts", :action => "index" })
 end
